00c195d8d6b93ba226d6046ce86017320ea58c4e
Robert Grimm <apparebit@gmail.com>
2024-05-16 09:04:36 -0400

    Make constructor more flexible, add documentation, and simplify updating colors

--- prettypretty/api/color.py
+++ prettypretty/api/color.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-from typing import cast, Literal, Self
+from typing import cast, Literal, overload, Self
 
 from ..color.apca import contrast, use_black_text, use_black_background
 from ..color.conversion import get_converter
@@ -19,14 +19,27 @@ class Color(ColorSpec):
     """
     A color object.
 
+    This class implements the high-level, object-oriented API for colors.
+
     Attributes:
         tag: identifies the color format or space
         coordinates: are the numerical components of the color
 
-    You can create a new color object either by passing a string that starts
-    with ``#``, ``rgb:``, or ``rgbi:`` and is followed by valid color components
-    or by passing both the color format or space tag and the coordinates. The
-    resulting color object is immutable.
+    Color's constructor supports a number of options for specifying the color
+    and its coordinates:
+
+        * From an existing :class:`.ColorSpec` or ``Color`` object
+        * From the textual representation of a color, using the ``#``,
+          ``rgb:``, or ``rgbi:`` prefix
+        * From a tag and tuple with coordinates
+        * From a tag and one or three coordinates
+
+    When invoked on an existing color specification or color object, the
+    constructor returns a new color object with the same tag and coordinates.
+    Hence, it effectively upgrades color specifications to full featured color
+    objects.
+
+    Like its parent class :class:`.ColorSpec`, this class is immutable.
     """
     __slots__ = ()
 
@@ -34,6 +47,7 @@ class Color(ColorSpec):
     def space(self) -> Space:
         """Get the color space for this color."""
         return resolve(self.tag)
+
 
     def __getattr__(self, name: str) -> float | Self:
         """
@@ -49,23 +63,29 @@ class Color(ColorSpec):
 
         raise AttributeError(f'color {self} has no attribute named "{name}"')
 
-    def __new__(
-        cls,
-        tag: str | ColorSpec | Self,
-        coordinates: None | CoordinateSpec = None,
-    ) -> Self:
-        if isinstance(tag, cls):
-            return tag
-        return super().__new__(cls)
+    # ----------------------------------------------------------------------------------
+
+
+    @overload
+    def __init__(self, tag: str | ColorSpec | Self) -> None:
+        ...
+
+    @overload
+    def __init__(self, tag: str, coordinates: CoordinateSpec) -> None:
+        ...
+
+    @overload
+    def __init__(self, tag: str, coordinates: float, c2: float, c3: float) -> None:
+        ...
 
     def __init__(
         self,
         tag: str | ColorSpec | Self,
-        coordinates: None | CoordinateSpec = None,
+        coordinates: None | float | CoordinateSpec = None,
+        c2: None | float = None,
+        c3: None | float = None,
     ) -> None:
-        if isinstance(tag, type(self)):
-            return
-        elif isinstance(tag, ColorSpec):
+        if isinstance(tag, ColorSpec):
             tag, coordinates = tag.tag, tag.coordinates
         elif coordinates is None:
             if tag.startswith('#'):
@@ -75,17 +95,44 @@ class Color(ColorSpec):
             elif tag.startswith('rgbi:'):
                 tag, coordinates = parse_x_rgbi(tag)
             else:
-                raise ValueError(f'unknown color format "{tag}"')
+                raise ValueError(f'"{tag}" is not a valid color')
+        elif c2 is not None:
+            assert isinstance(coordinates, float) and c3 is not None
+            coordinates = coordinates, c2, c3
 
         object.__setattr__(self, 'tag', tag)
         object.__setattr__(self, 'coordinates', coordinates)
         self.__post_init__()
 
+
+    def update(
+        self,
+        c1: None | float,
+        c2: None | float = None,
+        c3: None | float = None,
+    ) -> Self:
+        """
+        Update this color.
+
+        Since color objects are immutable, this method returns a new color. That
+        color has the same color format or space as this color. Its coordinates
+        are the arguments to this method, except that this color's coordinates
+        fill null arguments.
+        """
+        if len(self.coordinates) == 1:
+            assert c2 is None and c3 is None
+            return type(self)(self.tag, cast(tuple[int], (c1 or self.coordinates[0],)))
+
+        b1, b2, b3 = self.coordinates
+        return type(self)(self.tag, (c1 or b1, c2 or b2, c3 or b3))
+
     # ----------------------------------------------------------------------------------
+    # Gamut and Clipping
 
     def is_in_gamut(self, epsilon: float = EPSILON) -> bool:
         """Determine whether this color is within gamut for its color space."""
         return self.space.is_in_gamut(*self.coordinates, epsilon)
+
 
     def clip(self, epsilon: float = 0) -> Self:
         """Clip this color to its color space's gamut."""
@@ -94,6 +141,7 @@ class Color(ColorSpec):
         return type(self)(self.tag, self.space.clip(*self.coordinates))
 
     # ----------------------------------------------------------------------------------
+    # Conversion to Other Formats and Color Spaces
 
     def to(self, target: str) -> Self:
         """Convert this color to the specified color format or space."""
@@ -102,6 +150,7 @@ class Color(ColorSpec):
         return type(self)(target, get_converter(self.tag, target)(*self.coordinates))
 
     # ----------------------------------------------------------------------------------
+    # Distance from Other Colors
 
     def distance(self, other: Self, *, version: Literal[1, 2] = 1) -> float:
         """
@@ -126,6 +175,7 @@ class Color(ColorSpec):
         return index
 
     # ----------------------------------------------------------------------------------
+    # Contrast Against Other Colors
 
     def contrast_against(self, background: Self) -> float:
         """
@@ -144,6 +194,7 @@ class Color(ColorSpec):
         """
         return use_black_text(*self.to('srgb').coordinates)
 
+
     def use_black_background(self) -> bool:
         """
         Determine whether to use a black or white background for text of this
@@ -152,10 +203,12 @@ class Color(ColorSpec):
         return use_black_background(*self.to('srgb').coordinates)
 
     # ----------------------------------------------------------------------------------
+    # Serialization to Text
 
     def __format__(self, format_spec: str) -> str:
         fmt, precision = parse_format_spec(format_spec)
         return stringify(self.tag, self.coordinates, fmt, precision)
+
 
     def __str__(self) -> str:
         return stringify(self.tag, self.coordinates)
