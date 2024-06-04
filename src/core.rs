6497d1e381605c690445eb845a6b29f5373cdd50
Robert Grimm <apparebit@gmail.com>
2024-06-04 04:31:01 -0400

    add Color struct

--- src/core.rs
+++ src/core.rs
@@ -564,6 +564,73 @@ pub fn delta_e_ok(coordinates1: &[f64; 3], coordinates2: &[f64; 3]) -> f64 {
 }
 
 // ====================================================================================================================
+// The Color Abstraction
+// ====================================================================================================================
+
+/// A color in one of the supported color spaces.
+#[derive(Copy, Clone, Debug)]
+pub struct Color {
+    space: ColorSpace,
+    coordinates: [f64; 3],
+}
+
+impl Color {
+    /// Create a new color with the given color space and coordinates.
+    pub fn new(space: ColorSpace, coordinates: [f64; 3]) -> Self {
+        Color { space, coordinates }
+    }
+
+    /// The color's color space.
+    pub fn space(&self) -> ColorSpace {
+        self.space
+    }
+
+    /// The color's coordinates.
+    pub fn coordinates(&self) -> &[f64; 3] {
+        &self.coordinates
+    }
+
+    /// Convert this color to the given color space.
+    pub fn to(&self, space: ColorSpace) -> Color {
+        Self {
+            space,
+            coordinates: convert(self.space, space, &self.coordinates),
+        }
+    }
+
+    /// Determine whether this and the other color represent the same color.
+    pub fn same(&self, other: Self) -> bool {
+        self.space == other.space && same_coordinates(
+            self.space, &self.coordinates, &other.coordinates
+        )
+    }
+
+    /// Determine the difference or distance between the two colors. This method
+    /// uses delta E Ok.
+    pub fn distance(&self, other: Self) -> f64 {
+        return delta_e_ok(&self.coordinates, &other.coordinates)
+    }
+
+    /// Determine whether this color is in-gamut for its color space.
+    pub fn in_gamut(&self) -> bool {
+        in_gamut(self.space, &self.coordinates)
+    }
+
+    /// Clip this color to the gamut of its color space.
+    pub fn clip(&mut self) {
+        clip(self.space, &mut self.coordinates)
+    }
+
+    /// Map this color into the gamut of its color space.
+    pub fn into_gamut(&self) -> Self {
+        Self {
+            space: self.space,
+            coordinates: into_gamut(self.space, &self.coordinates),
+        }
+    }
+}
+
+// ====================================================================================================================
 
 #[cfg(test)]
 mod test {
