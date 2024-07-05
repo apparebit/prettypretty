64320021b376ca276a4296761d214c67891a0754
Robert Grimm <apparebit@gmail.com>
2024-07-05 00:47:17 -0400

    improve spacing

--- runtest.py
+++ runtest.py
@@ -23,28 +23,38 @@ if __name__ == "__main__":
         stream.flush()
 
     println(styled.h1("1. Setup"))
+
     println(styled.h2("PYTHONIOENCODING"))
     println(os.environ.get("PYTHONIOENCODING", "n/a"))
+
     if os.name == "nt":
         println(styled.h2("PYTHONLEGACYWINDOWSFSENCODING"))
         println(os.environ.get("PYTHONLEGACYWINDOWSFSENCODING", "n/a"))
+
         println(styled.h2("PYTHONLEGACYWINDOWSSTDIO"))
         println(os.environ.get("PYTHONLEGACYWINDOWSSTDIO", "n/a"))
+
     println(styled.h2("PYTHONUTF8"))
     println(os.environ.get("PYTHONUTF8", "n/a"))
+
     println(styled.h2("Standard Out/Err Encoding"))
     println(sys.stdout.encoding)
     println(sys.stderr.encoding)
+
     println(styled.h2("Python"))
     println(f"{sys.executable}")
+
     println(styled.h2("Python Prefix"))
     println(f"{sys.prefix}")
     println(f"{sys.base_prefix}")
+
     println(styled.h2("Python Path"))
     for path in sys.path:
         println(f"{path}")
+
     println(styled.h2("Current Directory"))
     println(f"{os.getcwd()}")
+
     println(styled.h2("Current Module"))
     println(f"{__file__}")
 
