427295d20a25b47eef486acbf2473bbec09d6494
Robert Grimm <apparebit@gmail.com>
2024-08-12 15:15:18 -0400

    uplift script to extract Python code from guide

---
+++ docs/book.py
@@ -0,0 +1,84 @@
+# This script is automatically generated from markdown sources.
+# Please do *not* edit.
+import os
+import sys
+sys.path.insert(0, os.path.abspath("."))
+
+
+def test0() -> None:
+    # overview/2020s.md:136
+    from prettypretty.color import Color, ColorSpace
+    oklch = Color.oklch(0.716, 0.349, 335.0)
+    p3 = oklch.to(ColorSpace.DisplayP3)
+    assert p3.in_gamut()
+    
+    not_srgb = oklch.to(ColorSpace.Srgb)
+    assert not not_srgb.in_gamut()
+    
+    srgb = not_srgb.to_gamut()
+    assert srgb == Color.srgb(1.0, 0.15942348587138203, 0.9222706101768445)
+
+
+def test1() -> None:
+    # overview/1970s.md:377
+    from prettypretty.color import Color
+    from prettypretty.color.style import AnsiColor, EmbeddedRgb, GrayGradient, TerminalColor
+    from prettypretty.color.style import TrueColor
+    red = AnsiColor.BrightRed
+    assert red.to_8bit() == 9
+    # What's the color value of ANSI red? We don't know!
+    
+    purple = EmbeddedRgb(3, 1, 4)
+    index = 16 + 3 * 36 + 1 * 6 + 4 * 1
+    assert index == 134
+    assert purple.to_8bit() == index
+    true_purple = TrueColor(*purple.to_24bit())
+    assert true_purple == TrueColor(175, 95, 215)
+    assert purple.to_color() == Color.from_24bit(175, 95, 215)
+    
+    gray = GrayGradient(18)
+    index = 232 + 18
+    assert index == 250
+    assert gray.level() == 18
+    assert gray.to_8bit() == index
+    true_gray = TrueColor(*gray.to_24bit())
+    assert true_gray == TrueColor(188, 188, 188)
+    assert gray.to_color() == Color.from_24bit(188, 188, 188)
+    
+    green = TerminalColor.from_8bit(71)
+    assert isinstance(green, TerminalColor.Rgb6)
+    also_green = green.color
+    assert also_green[0] == 1
+    assert also_green[1] == 3
+    assert also_green[2] == 1
+    true_green = TrueColor(*green.try_to_24bit())
+    assert true_green == TrueColor(95, 175, 95)
+    assert also_green.to_color() == Color.from_24bit(95, 175, 95)
+
+
+def test2() -> None:
+    # overview/integration.md:211
+    from prettypretty.color import Color, OkVersion
+    from prettypretty.color.style import AnsiColor, EmbeddedRgb, Fidelity
+    from prettypretty.color.style import TerminalColor, TrueColor
+    from prettypretty.color.trans import Translator
+    from prettypretty.theme import VGA
+    red = VGA[AnsiColor.BrightRed.to_8bit() + 2]
+    assert red == Color.srgb(1.0, 0.333333333333333, 0.333333333333333)
+    
+    translator = Translator(OkVersion.Revised, VGA)
+    also_red = translator.resolve(AnsiColor.BrightRed)
+    assert red == also_red
+    
+    black = translator.to_ansi(Color.srgb(0.15, 0.15, 0.15))
+    assert black == AnsiColor.Black
+    
+    maroon = translator.cap(TrueColor(148, 23, 81), Fidelity.EightBit)
+    assert maroon == TerminalColor.Rgb6(EmbeddedRgb(2, 0, 1))
+
+
+if __name__ == "__main__":
+    test0()
+    test1()
+    test2()
+    print("happy, happy, joy, joy!")
--- docs/pyextractor.py
+++ docs/pyextractor.py
@@ -1,33 +1,93 @@
 #!.venv/bin/python
 
 import json
+from pathlib import Path
 import re
 import sys
+import textwrap
+from typing import Any
 
 
 PYTHON_BLOCK = re.compile(r"(?:^|\n)```py(?:thon)?\n([^`]*)```")
+PYTHON_HIDDEN_LINE = re.compile("(?:^|\n)~")
 
 
-if __name__ == "__main__":
+def collect(items: list[Any]) -> list[str]:
+    blocks: list[str] = []
+
+    def traverse(item: Any) -> None:
+        if item is None:
+            return
+
+        chapter = item.get("Chapter")
+        if chapter is None:
+            return
+
+        content = chapter.get("content")
+        if content is None:
+            return
+
+        source_path = chapter.get("source_path")
+        if source_path is None:
+            source_path = ""
+
+        previous_end = 0
+        lines_so_far = 0
+
+        for block in PYTHON_BLOCK.finditer(content):
+            start_line = lines_so_far + content.count("\n", previous_end, block.start(1))
+            text = block.group(1)
+
+            blocks.append(f"# {source_path}:{start_line}\n{text}")
+
+            previous_end = block.end()
+            lines_so_far = start_line + text.count("\n")
+
+        for sub_item in chapter.get("sub_items", []):
+            traverse(sub_item)
+
+    for item in items:
+        traverse(item)
+
+    return blocks
+
+
+def main() -> None:
     if len(sys.argv) >= 2 and sys.argv[1] == "supports":
         sys.exit(0)
 
-    context, book = json.load(sys.stdin)
+    _, book = json.load(sys.stdin)
+    blocks = collect(book.get("sections", []))
 
-    all_blocks: list[str] = []
-    for section in book.get("sections", []):
-        chapter = section.get("Chapter", None)
-        if chapter is None:
-            continue
-        content = chapter.get("content", "")
-        blocks = PYTHON_BLOCK.findall(content)
-        all_blocks.extend(blocks)
+    with open(Path(__file__).parent / "book.py", mode="w", encoding="utf8") as file:
+        file.write(textwrap.dedent(
+            """\
+            # This script is automatically generated from markdown sources.
+            # Please do *not* edit.
+            import os
+            import sys
+            sys.path.insert(0, os.path.abspath("."))\n\n
+            """
+        ))
 
-    with open("pyextract.py", mode="w", encoding="utf8") as file:
-        for block in all_blocks:
-            file.write(block)
-            file.write("\n\n")
-        file.write("print(\"happy, happy, joy, joy!\")")
+        for index, block in enumerate(blocks):
+            file.write(f"def test{index}() -> None:\n    ")
+            file.write(
+                PYTHON_HIDDEN_LINE
+                .sub("\n", block)
+                .replace("\n", "\n    ")
+                .strip()
+            )
+            file.write("\n\n\n")
+
+        file.write('if __name__ == "__main__":\n')
+        for index in range(len(blocks)):
+            file.write(f"    test{index}()\n")
+        file.write('    print("happy, happy, joy, joy!")\n')
         file.flush()
 
     json.dump(book, sys.stdout)
+
+
+if __name__ == "__main__":
+    main()
