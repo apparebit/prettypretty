1c8cbfc360b427faf41f5ade19982805e334840e
Robert Grimm <apparebit@gmail.com>
2024-11-13 05:02:15 -0500

    fix formatting

--- benches/query.rs
+++ benches/query.rs
@@ -4,52 +4,32 @@ use prettypretty::theme;
 
 pub fn run_benchmarks(c: &mut Criterion) {
     unsafe { terminal().connect() }.unwrap();
-
-    // Compare the query functions without the overhead of object creation.
     let mut group = c.benchmark_group("theme-query");
     group.sample_size(10);
 
-    group.bench_function(
-        "1-loop",
-        | b | {
-            b.iter_batched(
-                || theme::prepare(false).expect("access to terminal"),
-                |(mut tty, mut scanner, mut theme)| {
-                    theme::query1(&mut tty, &mut scanner, &mut theme)
-                },
-                BatchSize::PerIteration,
-            )
-        },
-    );
-
-    group.bench_function(
-        "2-loops",
-        | b | {
-            b.iter_batched(
-                || theme::prepare(false).expect("access to terminal"),
-                |(mut tty, mut scanner, mut theme)| {
-                    theme::query2(&mut tty, &mut scanner, &mut theme)
-                },
-                BatchSize::PerIteration,
-            )
-        },
-    );
-
-    group.bench_function(
-        "3-loops",
-        | b | {
-            b.iter_batched(
-                || theme::prepare(false).expect("access to terminal"),
-                |(mut tty, mut scanner, mut theme)| {
-                    theme::query3(&mut tty, &mut scanner, &mut theme)
-                },
-                BatchSize::PerIteration,
-            )
-        },
-    );
+    group.bench_function("1-loop", |b| {
+        b.iter_batched(
+            || theme::prepare(false).expect("access to terminal"),
+            |(mut tty, mut scanner, mut theme)| theme::query1(&mut tty, &mut scanner, &mut theme),
+            BatchSize::PerIteration,
+        )
+    });
+    group.bench_function("2-loops", |b| {
+        b.iter_batched(
+            || theme::prepare(false).expect("access to terminal"),
+            |(mut tty, mut scanner, mut theme)| theme::query2(&mut tty, &mut scanner, &mut theme),
+            BatchSize::PerIteration,
+        )
+    });
+    group.bench_function("3-loops", |b| {
+        b.iter_batched(
+            || theme::prepare(false).expect("access to terminal"),
+            |(mut tty, mut scanner, mut theme)| theme::query3(&mut tty, &mut scanner, &mut theme),
+            BatchSize::PerIteration,
+        )
+    });
 
     group.finish();
-
     terminal().disconnect();
 }
 
