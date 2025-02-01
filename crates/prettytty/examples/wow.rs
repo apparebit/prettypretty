07cd5c2acfab92c9ed27ba45cd36bc01890322ee
Robert Grimm <apparebit@gmail.com>
2025-02-01 17:45:38 -0500

    make the wow box look more wow

--- crates/prettytty/examples/wow.rs
+++ crates/prettytty/examples/wow.rs
@@ -1,12 +1,22 @@
 use prettytty::{
-    cmd::{Format, ResetStyle, SetForeground8},
+    cmd::{Format, ResetStyle, SetBackground8, SetDefaultBackground, SetForeground8},
     fuse_sgr, Sgr,
 };
 
+const WOW: u8 = 124;
+const BOX: u8 = 202;
+const BG: u8 = 230;
+
 fn main() {
     println!(
-        "\n    {}┏━━━━━━┓\n    ┃ Wow! ┃\n    ┗━━━━━━┛{}\n",
-        fuse_sgr!(Format::Bold, SetForeground8::<202>),
+        "\n    {}┏━━━━━━┓{}\n    {}┃{} Wow! {}┃{}\n    {}┗━━━━━━┛{}\n",
+        fuse_sgr!(SetBackground8::<BG>, SetForeground8::<BOX>),
+        SetDefaultBackground,
+        SetBackground8::<BG>,
+        fuse_sgr!(SetForeground8::<WOW>, Format::Bold),
+        fuse_sgr!(SetForeground8::<BOX>, Format::Regular),
+        SetDefaultBackground,
+        SetBackground8::<BG>,
         ResetStyle
     );
 }
